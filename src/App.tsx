1bd5c9d459bca6d8408007f12b5aec8fcae83d0d
onlyhamzashahid <hamza.7285618@gmail.com>
2022-05-01 09:29:37 +0500

    Profile Component

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,15 @@ import Profile from './components/Profile'
 function App() {
   return (
     <div className="App">
-      <Profile name = "hamza" age={20}/>  
+      <Profile  age={12} />  
+      <Profile name = "Shahid" age={40} status="Designer"/>  
+      <Profile name = "Hamza" age={20} status="Developer"/>  
+      <Profile name = "Shawn" age={21} status="Developer"/> 
+      <Profile name = "Jhon" age={25} status="Developer">
+        <span style={{color:"green"}}>Salary - 5cr</span>
+      </Profile>
     </div>
-  );
+  ); 
 }
 
 export default App;
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { } from 'react'
 
 interface ProfileProps{
-    name: string
+    name?: string
     age: number
+    // status?: string
+    status?: "Designer" | "Developer" | "Unknown"
+    children?: React.ReactNode
 }
 
-const Profile = (props: ProfileProps) => {
+const Profile = ({name,age, status,children}: ProfileProps) => {
   return (
       <div style={{border:"2px solid",margin: "2px"}}>
-          <h1>Name - {props.name}</h1>
-          <h1>Age - {props.age}</h1>
+          <h1>Name - {name || "User"}</h1>
+          <h1>Age - {age}</h1>
+          <h1>Status - {status || "Unknown"}</h1>
+          <h1>{children}</h1>
       </div>
   )
 }
